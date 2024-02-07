c8afd6c3ed6fd85ca6ac5aac6d74921c88750f7d
Juliana <jumiranda0501@gmail.com>
2024-02-07 17:25:40 -0300

    Add location page style

--- src/page-location/images-location.js
+++ src/page-location/images-location.js
@@ -1,6 +1,13 @@
 'use-strict';
 
-import { createPicture } from '../utils/img-utils';
+import { createPicture, setObserver } from '../utils/img-utils';
+
+// svg
+import ArrowLeft from '../assets/images/icon-arrow-left.svg';
+import Logo from '../assets/images/logo-dark.svg';
+
+document.getElementById('link-arrow').src = ArrowLeft;
+document.getElementById('footer-logo').src = Logo;
 
 // mobile
 import Map from '../assets/images/mobile/image-map.png';
@@ -14,14 +21,23 @@ import Map_tablet_2x from '../assets/images/tablet/image-map@2x.png';
 import Map_desk from '../assets/images/desktop/image-map.png';
 import Map_desk_2x from '../assets/images/desktop/image-map@2x.png';
 
+// tiny
+import map_tn from '../assets/images/tiny/tn-image-map.png';
+
+// Create objects with images variations
+
 const map = {
     mob: Map,
     mob2x: Map_2x,
     tablet: Map_tablet,
     tablet2x: Map_tablet_2x,
     desk: Map_desk,
-    desk2x: Map_desk_2x
+    desk2x: Map_desk_2x,
+    tn: map_tn
 };
 
 // Set pictures
 createPicture('map-img-container', map, '');
+
+// Init observer
+setObserver();
